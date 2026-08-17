f559dca8f0ab645439a6c73ed25493c5c813a5d2
novakvova <novakvova@gmail.com>
2026-08-17 16:56:02 +0300

    Is good working Userenitty

---
+++ 1.Begin/SpringAPI/src/main/java/org/example/conrollers/AccountController.java
@@ -0,0 +1,12 @@
+package org.example.conrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+    @GetMapping("/login")
+    public String login() {
+        return "account/login";
+    }
+}
