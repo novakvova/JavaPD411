5db9da70daaa9c74c8c35cff86d2e0f39c48ab71
novakvova <novakvova@gmail.com>
2026-08-21 16:17:48 +0300

    Regiser user good

--- 1.Begin/SpringAPI/src/main/java/org/example/config/DatabaseSeeder.java
+++ 1.Begin/SpringAPI/src/main/java/org/example/config/DatabaseSeeder.java
@@ -1,4 +1,35 @@
 package org.example.config;
 
-public class DatabaseSeeder {
+
+import org.example.entities.UserEntity;
+import org.example.repositories.IUserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private final IUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseSeeder(IUserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        String testEmail = "novakvova@gmail.com";
+
+        if (userRepository.findByEmail(testEmail).isEmpty()) {
+            UserEntity user = new UserEntity();
+            user.setEmail(testEmail);
+            user.setUsername(testEmail); // Обов'язково робимо username = email
+            user.setPassword(passwordEncoder.encode("123456"));
+
+            userRepository.save(user);
+            System.out.println(">>> SEED: Користувача засіяно: "+testEmail+" / 123456");
+        }
+    }
 }
--- 1.Begin/SpringAPI/src/main/java/org/example/dtos/account/RegisterDto.java
+++ 1.Begin/SpringAPI/src/main/java/org/example/dtos/account/RegisterDto.java
@@ -1,4 +1,18 @@
 package org.example.dtos.account;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RegisterDto {
+    @NotBlank
+    private String username;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String confirmPassword;
 }
